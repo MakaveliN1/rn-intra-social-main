d5cc2fc054f2c905778ac26490455eed7dcd0800
Adnan Fahad <adnanfahad545@gmail.com>
2023-12-28 20:13:17 +0500

    Add Edit Profile Feature

--- app/screens/UserProfile.js
+++ app/screens/UserProfile.js
@@ -1,7 +1,23 @@
+/* eslint-disable prettier/prettier */
 import React, {useState, useEffect} from 'react';
-import {View, Text, Image, StyleSheet} from 'react-native';
-import {FirebaseAuth, FirebaseFireStore} from '../../firebaseConfig'; // Import FirebaseAuth and Firestore
-import {collection, query, where, getDocs} from 'firebase/firestore';
+import {
+  View,
+  Text,
+  Image,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+  Alert,
+} from 'react-native';
+import {FirebaseAuth, FirebaseFireStore} from '../../firebaseConfig';
+import {
+  collection,
+  query,
+  where,
+  getDocs,
+  doc,
+  updateDoc,
+} from 'firebase/firestore';
 
 const UserProfile = () => {
   const imgUrl = 'https://placekitten.com/640/360';
@@ -11,29 +27,32 @@ const UserProfile = () => {
     age: '',
     gender: '',
     phone: '',
-    photoURL: imgUrl, // Add a field for the photo URL if you store this in Firestore
+    photoURL: imgUrl,
+  });
+
+  const [editableFields, setEditableFields] = useState({
+    name: false,
+    age: false,
+    gender: false,
+    phone: false,
   });
 
   useEffect(() => {
     const fetchUserData = async () => {
-      // Get the current user from FirebaseAuth
       const user = FirebaseAuth.currentUser;
       if (user) {
-        // Create a query against the users collection
         const usersRef = collection(FirebaseFireStore, 'users');
         const q = query(usersRef, where('uid', '==', user.uid));
         const querySnapshot = await getDocs(q);
         if (!querySnapshot.empty) {
-          // Assuming there's only one document for each user
           const userDoc = querySnapshot.docs[0].data();
-          // console.log(userDoc)
           setUserData({
             name: userDoc.name,
             email: userDoc.email,
             age: userDoc.age,
             gender: userDoc.gender,
             phone: userDoc.phone,
-            photoURL: userDoc.photoURL, // Use FirebaseAuth photoURL or a placeholder
+            photoURL: userDoc.photoURL,
           });
         }
       }
@@ -42,16 +61,63 @@ const UserProfile = () => {
     fetchUserData();
   }, []);
 
+  const handleEdit = field => {
+    setEditableFields({...editableFields, [field]: true});
+  };
+
+  const handleSave = async field => {
+    try {
+      const userRef = doc(
+        FirebaseFireStore,
+        'users',
+        FirebaseAuth.currentUser.uid,
+      );
+
+      await updateDoc(userRef, {[field]: userData[field]});
+
+      setEditableFields({...editableFields, [field]: false});
+      Alert.alert('Success', `Your ${field} has been updated successfully!`);
+    } catch (error) {
+      console.error(`Error updating ${field}:`, error);
+      Alert.alert('Error', `Failed to update ${field}. Please try again.`);
+    }
+  };
+
+  const renderEditableField = (field, label) => (
+    <View style={styles.editableFieldContainer}>
+      <Text style={styles.label}>{label}</Text>
+      {editableFields[field] ? (
+        <View style={styles.inputContainer}>
+          <TextInput
+            style={styles.input}
+            value={userData[field]}
+            onChangeText={text => setUserData({...userData, [field]: text})}
+          />
+          <TouchableOpacity onPress={() => handleSave(field)}>
+            <Text style={styles.saveButton}>Save</Text>
+          </TouchableOpacity>
+        </View>
+      ) : (
+        <View style={styles.fieldContainer}>
+          <Text style={styles.fieldLabel}>{userData[field]}</Text>
+          <TouchableOpacity onPress={() => handleEdit(field)}>
+            <Text style={styles.editButton}>Edit</Text>
+          </TouchableOpacity>
+        </View>
+      )}
+    </View>
+  );
+
   return (
     <View style={styles.container}>
       {userData.photoURL && (
         <Image source={{uri: userData.photoURL}} style={styles.avatar} />
       )}
-      <Text style={styles.name}>{userData.name || 'No name'}</Text>
+      {renderEditableField('name', 'Name')}
       <Text style={styles.email}>{userData.email}</Text>
-      <Text style={styles.detail}>Age: {userData.age || 'N/A'}</Text>
-      <Text style={styles.detail}>Gender: {userData.gender || 'N/A'}</Text>
-      <Text style={styles.detail}>Phone: {userData.phone || 'N/A'}</Text>
+      {renderEditableField('age', 'Age')}
+      {renderEditableField('gender', 'Gender')}
+      {renderEditableField('phone', 'Phone')}
     </View>
   );
 };
@@ -69,21 +135,50 @@ const styles = StyleSheet.create({
     borderRadius: 75,
     marginBottom: 20,
   },
-  name: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    marginBottom: 8,
-  },
   email: {
     fontSize: 16,
     color: 'gray',
-    marginBottom: 4,
+    marginBottom: 10,
   },
-  detail: {
+  editableFieldContainer: {
+    marginBottom: 15,
+    width: '80%',
+  },
+  fieldContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  label: {
     fontSize: 16,
-    marginBottom: 4,
+    marginBottom: 5,
+    fontWeight: 'bold',
   },
-  // Add styles for other user data fields
+  fieldLabel: {
+    fontSize: 16,
+    marginRight: 10,
+  },
+  editButton: {
+    color: 'blue',
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  input: {
+    flex: 1,
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    borderRadius: 5,
+    marginRight: 10,
+    paddingLeft: 5,
+  },
+  saveButton: {
+    color: 'green',
+    fontWeight: 'bold',
+    fontSize: 16,
+  },
 });
 
 export default UserProfile;
